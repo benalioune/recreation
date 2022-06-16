dedaa0eef80052d25db34c433192485fdd81f02b
diallo <benalioune6@gmail.com>
2022-06-16 11:53:09 +0200

    adding access

--- application/config/config.php
+++ application/config/config.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-define('URL', 'http://127.0.0.1/recreation');
+define('URL', 'http://127.0.0.1/recreation/');
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'php-mvc');
+define('DB_NAME', 'convoiturage');
 define('DB_USER', 'root');
 define('DB_PASS', '');
