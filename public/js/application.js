90b60bb82bd48c720c7e5ba5415cfd7e4c816c5f
diallo <benalioune6@gmail.com>
2022-06-16 11:16:35 +0200

    architecture

---
+++ public/js/application.js
@@ -0,0 +1,10 @@
+
+$(function() {
+
+    if ($('#javascript-header-demo-box').length != 0) {
+        $('#javascript-header-demo-box').hide();
+        $('#javascript-header-demo-box').text('Hello from JavaScript! This line has been added by public/js/application.js');
+        $('#javascript-header-demo-box').css('color', 'green');
+        $('#javascript-header-demo-box').fadeIn('slow');
+    }
+});
