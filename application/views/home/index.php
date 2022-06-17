adf447b885a822af5c0c834c447201137a7870c8
Aneina <68449108+Aneina@users.noreply.github.com>
2022-06-17 11:07:40 +0200

    Update index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,65 @@
-<div class="container">
-    <h2>You are in the View: application/views/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
+<html> 
+	<body>
+<title>accueil</title>
+
+    
+<div class="img-home">
+   <img class="clipped" src="classe1.jpg" alt=""/>
+   <div class="texte_centrer" style="opacity: 0.8"><h2 style="opacity: 1;">Bienvenue à toi Pot de colle !</h2> </div>
 </div>
+
+<div id="container">
+  <div id="a">
+    <div id="texta">
+      <h3>Horaires</h3><br/>
+      <p>Matin :<br/>
+      8h20 - Arrivée des enfants et débuts de cours<br/>
+      11h50 - Déjeuner</p>
+      <p>Après-midi :<br/>
+      13h50 - Reprise de l’école <br/>
+      16h30 - Fin de journée + début garderie/études<br/>
+      18h - Fermeture de la garderie</p>
+    </div>
+
+  </div>
+  <div id="b">
+  <div id="titreb">
+    <h3>Informations</h3><br/>
+    <div id="textb">
+      <p  style="font-weight: bold "> Sortie au musée du Louvre  du 20 juin</p>
+      <p>Départ à 9h - Retour 16h30 <br/>
+      Ne pas oublier de prendre son pique-nique</p>
+    </div>
+    <div id="textb">
+      <p style="font-weight: bold ">Grève de Cantine Mardi 21</p>
+      <p>Ne pas oublier de prendre son repas</p>
+    </div>
+  </div>
+  </div>
+</div>
+
+		
+	</body>		
+		
+	<script>
+/*Execute a function that will execute an image compare function for each element with the img-comp-overlay class:*/
+initComparisons();
+</script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip();   
+		});
+		
+	</script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166118568-1"></script>
+	<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-166118568-1');
+	</script>
+
+</html>
