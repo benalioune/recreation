9a83058124a88408daad4bb48e8d80dc0a4574da
Emilie Coatelant <emiliecoatelant@outlook.fr>
2022-06-17 13:14:08 +0200

    config

--- application/config/config-serveur.php
+++ application/config/config-serveur.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-define('URL', 'http://localhost:8888/recreation/');
+define('URL', 'http://projets.iut-bobigny.univ-paris13.fr/~recreation/');
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', '');
-define('DB_USER', 'ecoatelant');
-define('DB_PASS', 'coatelant');
+define('DB_USER', 'recreation');
+define('DB_PASS', 'tz+bWyWVWien');
